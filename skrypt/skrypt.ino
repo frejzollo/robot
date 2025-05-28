6a6dc3f8d909c0b9f87be959afe7fe195b493791
frejzollo <kacper.kusi@interia.pl>
2025-05-28 20:35:25 +0200

    s
    s

--- skrypt/skrypt.ino
+++ skrypt/skrypt.ino
@@ -11,6 +11,11 @@ int analogValues[7]; // wartosci z sensorow
 int readErrorBlack = 7; // podloga
 int readErrorWhite = 7; // linia
 
+
+bool blackCali = false; //czy skalibrowano sensory na linie
+bool whiteCali = false; //czy skalibrowano sensory na powierzchnie
+
+
 void setup() {
   Serial.begin(9600);  // Uruchomienie komunikacji szeregowej
   for (int i = 0; i < 7; i++) {
@@ -24,7 +29,43 @@ void loop() {
     for (int i = 0; i < 7; i++) {
     analogValues[i] = analogRead(analogPins[i]);
   }
-  basicInfo();
+
+  if(digitalRead(button)){
+    mode += 1;
+    delay(400); //debounce guzika
+  }
+
+  //mod 1: jednorazowe zczytanie aktualnych odczytów czujników i przypisanie ich jako wartości odpowiadających linii
+  if(mode == 1 && !blackCali){
+    for (int i = 0; i < 7; i++) {
+      blackLevels[i] = analogRead(analogPins[i]);
+    }
+    blackCali = true;
+  }
+  
+  //mod 2: jednorazowe zczytanie aktualnych odczytów czujników i przypisanie ich jako wartości odpowiadających powierzchni
+  if(mode == 2 && !whiteCali){
+    for (int i = 0; i < 7; i++) {
+      whiteLevels[i] = analogRead(analogPins[i]);
+    }
+    whiteCali = true;
+  }
+
+  //zczytywanie wartości czujników po kalibracji: powierzchnia = 1, linia = -1, niepewny odczyt = 0
+  if(whiteCali && blackCali){
+    for (int i = 0; i < 7; i++) {
+      if(abs(blackLevels[i] - analogValues[i]) < readErrorBlack){
+        caliValues[i] = -1;
+      } else if(abs(whiteLevels[i] - analogValues[i]) < readErrorWhite || analogValues[i] > whiteLevels[i]){ // or dlatego, że kalibracja mogła być w cieniu czy coś tam...}
+        caliValues[i] = 1;
+      } else{
+        caliValues[i] = 0;
+      }
+    }
+  }
+
+  //basicInfo();
+  levelsInfo();
 }
 
 //informacja na serial: stan guzika / wartość pokrętła / [wartości sensorów] / tryb pracy (mode)
@@ -43,4 +84,27 @@ void basicInfo(){
   delay(1000);
 }
 
+void levelsInfo(){
 
+  Serial.print("[");
+  for (int i = 0; i < 7; i++) {
+    Serial.print(analogValues[i]);
+    Serial.print(i < 6 ? ", " : "] / ");
+  }
+  Serial.print("[");
+  for (int i = 0; i < 7; i++) {
+    Serial.print(blackLevels[i]);
+    Serial.print(i < 6 ? ", " : "] / ");
+  }
+  Serial.print("[");
+  for (int i = 0; i < 7; i++) {
+    Serial.print(whiteLevels[i]);
+    Serial.print(i < 6 ? ", " : "] / ");
+  }
+  Serial.print("[");
+  for (int i = 0; i < 7; i++) {
+    Serial.print(caliValues[i]);
+    Serial.print(i < 6 ? ", " : "] \n");
+  }
+
+}
