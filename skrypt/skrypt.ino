926863d5d3c03f1954176b5b98c6af54f51e3c22
frejzollo <kacper.kusi@interia.pl>
2025-05-28 19:37:07 +0200

    s
    s

--- skrypt/skrypt.ino
+++ skrypt/skrypt.ino
@@ -1,4 +1,4 @@
-const float VREF = 3.3;  // Zmieniamy z 5.0 na 3.3V
+const float V_REF = 3.3;  // Zmieniamy z 5.0 na 3.3V
 
 void setup() {
   Serial.begin(9600);  // Uruchomienie komunikacji szeregowej
@@ -11,7 +11,7 @@ void loop() {
 void basic_info() {
   for (int i = 0; i <= 7; i++) {
     int value = analogRead(i);  // Odczyt z wejścia analogowego
-    float voltage = (value / 1023.0) * VREF;  // Przeliczenie na napięcie
+    float voltage = (value / 1023.0) * V_REF;  // Przeliczenie na napięcie
 
     Serial.print("A");
     Serial.print(i);
