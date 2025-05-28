4264e8e7bc08bfa33c4f68f5b8c933bffe225868
frejzollo <kacper.kusi@interia.pl>
2025-05-28 20:05:20 +0200

    s

--- skrypt/skrypt.ino
+++ skrypt/skrypt.ino
@@ -1,10 +1,16 @@
+const int speedsetter = A0;
+const int button = 13;
+
+int Sensors[7] = {A1, A2, A3, A4, A5, A6, A7};
+int AnalogValues[7];
+int CaliValues[7];
+
 void setup() {
   Serial.begin(9600);  // Uruchomienie komunikacji szeregowej
 }
 
 void loop() {
 basic_info();
-
 }
 
 void basic_info()
@@ -20,3 +26,5 @@ void basic_info()
   Serial.println();  // Nowa linia po wszystkich odczytach
   delay(500);        // Małe opóźnienie dla czytelności
 };
+
+
