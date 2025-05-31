adb23de5a3f610d262a11a5d52624b7401620cb9
damndanio <olaf3ds@gmail.com>
2025-05-31 20:06:46 +0200

    Update skryptplus.ino

--- skrypt/skryptplus/skryptplus.ino
+++ skrypt/skryptplus/skryptplus.ino
@@ -31,8 +31,8 @@ const int L2 = 7;
 const int L1 = 6;
 //Motor prawy
 const int ENR = 10;
-const int R2 = 9;
-const int R1 = 8;
+const int R2 = 8;
+const int R1 = 9;
 
 //TABLICE WARTOŚCI SENSORÓW____________________________________________
 
